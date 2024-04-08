a36905d5da828d9503a9a2603577353e944262ea
fjolivo <fjolivo@gmail.com>
2024-04-08 16:09:00 -0400

    Update Sidebar.tsx

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -39,33 +39,33 @@ const Sidebar = () => {
                                 </li>
                             )
                         })}
-                        </ul>
+                    </ul>
 
                         
-                        <ul className="sidevar-nav_elements">
-                        {navLinks.slice(6).map((link) =>{
-                            const isActive = link.route === pathname
+                    <ul className="sidevar-nav_elements">
+                    {navLinks.slice(6).map((link) =>{
+                        const isActive = link.route === pathname
 
-                            return (
-                                <li key={link.route} className={`sidebar-nav_element group ${
-                                    isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
-                                }`}>
-                                    <Link className="sidebar-link" href={link.route}>
-                                        <Image
-                                            src={link.icon}
-                                            alt="logo"
-                                            width={24}
-                                            height={24}
-                                            className={`$isActive && 'brightness-200'}`}
-                                        />
-                                        {link.label}
-                                    </Link>
-                                </li>
-                            )
-                        })}
-                            <li className="flex-center cursor-pointer gap-2 p-4">
-                                <UserButton afterSignOutUrl='/' showName />
+                        return (
+                            <li key={link.route} className={`sidebar-nav_element group ${
+                                isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                            }`}>
+                                <Link className="sidebar-link" href={link.route}>
+                                    <Image
+                                        src={link.icon}
+                                        alt="logo"
+                                        width={24}
+                                        height={24}
+                                        className={`$isActive && 'brightness-200'}`}
+                                    />
+                                    {link.label}
+                                </Link>
                             </li>
+                        )
+                    })}
+                    <li className="flex-center cursor-pointer gap-2 p-4">
+                        <UserButton afterSignOutUrl='/' showName />
+                    </li>
 
                     </ul>
                 </SignedIn>
