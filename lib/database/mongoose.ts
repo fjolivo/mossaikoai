50a2c748a41f69eeec4f3a025d5f0e2c9743c1e6
fjolivo <fjolivo@gmail.com>
2024-04-09 00:22:54 -0400

    Schema models created

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,13 +1,13 @@
 import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/test';
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
     conn: Mongoose | null;
     promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = {global as any).mongoose
+let cached: MongooseConnection = (global as any).mongoose
 
 if (!cached) {
     cached = (global as any).mongoose = {
@@ -16,15 +16,16 @@ if (!cached) {
 }
 
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn;
+        if (cached.conn) return cached.conn;
 
-    if (!MONGODB_URL) throw new Error('Mising MONGODB_URL'); 
+        if (!MONGODB_URL) throw new Error('Mising MONGODB_URL'); 
 
-    cached.promise =
-    cached.promise || mongoose.connect(MONGODB_URL, { 
-        dbName: 'Mossaiko AI', bufferCommands: false
-    })
+        cached.promise =
+        cached.promise || mongoose.connect(MONGODB_URL, { 
+            dbName: 'Mossaiko AI', bufferCommands: false
+        });
 
-    cached.conn = await cached.promise;
+        cached.conn = await cached.promise;
 
-    return cached.conn;
+        return cached.conn;
+    } 
