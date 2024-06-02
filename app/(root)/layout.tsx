0f04d95382e08d5d9443ac15cea536e564c3dd3b
fjolivo <fjolivo@gmail.com>
2024-06-02 18:37:16 -0400

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -19,20 +19,20 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <main className="root">
-      <div className="absolute inset-0 -z-20 bg-orange-50 " aria-hidden="true"></div> 
+      <div className="absolute inset-0 -z-20 bg-orange-20 " aria-hidden="true"></div> 
 
       <div
         className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
         aria-hidden="true"
       >
         <div
-          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#ffd3bb] opacity-80 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-transparent to-[#ffd4bc] opacity-80 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           style={{
             clipPath: clipPath1,
           }}
         />
         <div
-          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#fff9f4] via-purple-500 to-pink-500 opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          className="relative left-[calc(50%+3rem)] w-[200vw] h-[50vh] -translate-x-1/2 bg-gradient-to-tr from-[#ffd4bc] to-transparent opacity-100 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
           style={{
             clipPath: clipPath2,
           }}
