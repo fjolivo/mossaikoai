4bd543fb796bc58aeb62afe1aa30ace354e94fa4
fjolivo <fjolivo@gmail.com>
2024-06-02 20:41:06 -0400

    Logo ver 2

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -12,75 +12,77 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="flex flex-row gap-4">
-        <Link href="/" className="header-logo">
-          <Image src="/assets/images/logo-text.svg" alt="logo" width={360} height={56}  />
-        </Link>
+        <div className="header-container">
+            <div className="flex flex-row gap-4">
+                <Link href="/" className="header-logo">
+                <Image src="/assets/images/logo-text.svg" alt="logo" width={360} height={56}  />
+                </Link>
 
-        <nav className="header-nav">
-          <SignedIn>
-            <ul className="header-nav_elements">
-              {navLinks.slice(0, 5).map((link) => {
-                const isActive = link.route === pathname
+                <nav className="header-nav">
+                <SignedIn>
+                    <ul className="header-nav_elements">
+                    {navLinks.slice(0, 5).map((link) => {
+                        const isActive = link.route === pathname
 
-                return (
-                  <li key={link.route} className={`header-nav_element group ${
-                    isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
-                  }`}>
-                    <Link className="header-link" href={link.route}>
-                      <Image 
-                        src={link.icon}
-                        alt="logo"
-                        width={24}
-                        height={24}
-                        className={`${isActive && 'brightness-200'}`}
-                      />
-                      {link.label}
-                    </Link>
-                  </li>
-                )
-              })}
-              </ul>
-
-
-            {/* <ul className="header-nav_elements">
-              {navLinks.slice(6).map((link) => {
-                const isActive = link.route === pathname
-
-                return (
-                  <li key={link.route} className={`header-nav_element group ${
-                    isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
-                  }`}>
-                    <Link className="header-link" href={link.route}>
-                      <Image 
-                        src={link.icon}
-                        alt="logo"
-                        width={24}
-                        height={24}
-                        className={`${isActive && 'brightness-200'}`}
-                      />
-                      {link.label}
-                    </Link>
-                  </li>
-                )
-              })}
-
-              <li className="flex-center cursor-pointer gap-2 p-4">
-                <UserButton afterSignOutUrl='/' showName />
-              </li>
-            </ul> */}
-          </SignedIn>
+                        return (
+                        <li key={link.route} className={`header-nav_element group ${
+                            isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                        }`}>
+                            <Link className="header-link" href={link.route}>
+                            <Image 
+                                src={link.icon}
+                                alt="logo"
+                                width={24}
+                                height={24}
+                                className={`${isActive && 'brightness-200'}`}
+                            />
+                            {link.label}
+                            </Link>
+                        </li>
+                        )
+                    })}
+                    </ul>
 
 
+                    {/* <ul className="header-nav_elements">
+                    {navLinks.slice(6).map((link) => {
+                        const isActive = link.route === pathname
+
+                        return (
+                        <li key={link.route} className={`header-nav_element group ${
+                            isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                        }`}>
+                            <Link className="header-link" href={link.route}>
+                            <Image 
+                                src={link.icon}
+                                alt="logo"
+                                width={24}
+                                height={24}
+                                className={`${isActive && 'brightness-200'}`}
+                            />
+                            {link.label}
+                            </Link>
+                        </li>
+                        )
+                    })}
+
+                    <li className="flex-center cursor-pointer gap-2 p-4">
+                        <UserButton afterSignOutUrl='/' showName />
+                    </li>
+                    </ul> */}
+                </SignedIn>
 
 
-          <SignedOut>
-            <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href="/sign-in">Login</Link>
-            </Button>
-          </SignedOut>
-        </nav>
-      </div>
+
+
+                <SignedOut>
+                    <Button asChild className="button bg-purple-gradient bg-cover">
+                    <Link href="/sign-in">Login</Link>
+                    </Button>
+                </SignedOut>
+                </nav>
+            </div>
+        </div>
     </header>
   )
 }
