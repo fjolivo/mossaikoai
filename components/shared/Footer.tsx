d327b16eb8f6f28977c2debba894946f859124be
fjolivo <fjolivo@gmail.com>
2024-06-07 17:24:33 -0400

    Styling changes in the footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -17,35 +17,34 @@ const Footer = () => {
             © 2024 Mossaiko AI. All rights reserved.
             </div> 
             <div className="footer-transformations">
-                <h3>Transformations</h3>
-            <SignedIn>
-                <ul>
-                {navLinks.slice(1, 4).map((link) => {
-                    return (
-                    <li key={link.route} className={`footer-nav_element`}>
-                        <Link className="header-link" href={link.route}>
-                            {link.label}
-                        </Link>
-                    </li>
-                    )
-                })}
-                </ul>
-            </SignedIn>
+                <SignedIn>
+                    <ul>
+                    {navLinks.slice(1, 4).map((link) => {
+                        return (
+                        <li key={link.route} className={`footer-nav_element`}>
+                            <Link className="header-link" href={link.route}>
+                                {link.label}
+                            </Link>
+                        </li>
+                        )
+                    })}
+                    </ul>
+                </SignedIn>
             </div>
             <div className="footer-profile">
-            <SignedIn>
-                <ul>
-                {navLinks.slice(4, 6).map((link) => {
-                    return (
-                    <li key={link.route} className={`footer-nav_element group`}>
-                        <Link className="header-link" href={link.route}>
-                            {link.label}
-                        </Link>
-                    </li>
-                    )
-                })}
-                </ul>
-            </SignedIn>
+                <SignedIn>
+                    <ul>
+                    {navLinks.slice(4, 6).map((link) => {
+                        return (
+                        <li key={link.route} className={`footer-nav_element`}>
+                            <Link className="header-link" href={link.route}>
+                                {link.label}
+                            </Link>
+                        </li>
+                        )
+                    })}
+                    </ul>
+                </SignedIn>
             </div>
 
                 {/* <Link href="/" className="header-logo">
